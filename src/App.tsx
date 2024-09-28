d8f1318cb050f149a2b64a66987f6b411189aa39
Joao Dantas <joaostadantas@gmail.com>
2024-09-28 17:48:28 -0300

    fix: env variable name

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ import { Detail } from './components/Detail';
 
 function App () {
   const isDevelopment = import.meta.env.VITE_ENV === 'development';
-  console.log(import.meta.env.VITE_ENV)
   const url = isDevelopment ?
     '/api/senior-fe-menu-challenge.json' :
     'https://cors-proxy4.p.rapidapi.com/?url=https%3A%2F%2Fcdn-dev.preoday.com%2Fsenior-fe-menu-challenge.json';
   const options = {
     method: 'GET',
     headers: {
-      'x-rapidapi-key': import.meta.env.RAPIDAPI_KEY,
+      'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
       'x-rapidapi-host': 'cors-proxy4.p.rapidapi.com'
     }
   };
