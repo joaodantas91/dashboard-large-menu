9eb9d9b2b521358b43efd97a96adf97d8c670037
Joao Dantas <joaostadantas@gmail.com>
2024-09-28 17:33:33 -0300

    fix: change proxy server for production

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,22 @@ import { Detail } from './components/Detail';
 
 function App () {
   const isDevelopment = import.meta.env.VITE_ENV === 'development';
-  const apiBaseUrl = isDevelopment ? '/api/' : '/.netlify/functions/fetchData?endpoint=';
-
+  const url = isDevelopment ?
+    '/api/senior-fe-menu-challenge.json' :
+    'https://cors-proxy4.p.rapidapi.com/?url=https%3A%2F%2Fcdn-dev.preoday.com%2Fsenior-fe-menu-challenge.json';
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': import.meta.env.RAPIDAPI_KEY,
+      'x-rapidapi-host': 'cors-proxy4.p.rapidapi.com'
+    }
+  };
   const [list, setList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const [selectedItem, setSelectedItem] = useState<Item | null>(null);
   useEffect(() => {
-    fetch(`${apiBaseUrl}senior-fe-menu-challenge.json`)
+    fetch(url, options)
       .then((res) => {
         return res.json();
       })
